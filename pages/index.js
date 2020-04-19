d6971402117c64496c47cc7f822f1bb09b3cbb2b
Aron Bergman <brgmn@icloud.com>
2020-04-19 17:04:22 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home page</h1>
+        </div>
+    )
+}
+
+export default Home;
